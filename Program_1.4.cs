750c4816a1e7c08a750b4a4c8557114e9587f280
Karina Zubko <karinazubko2017@gmail.com>
2023-03-04 20:46:45 +0100

    Hello World

---
+++ Program_1.4.cs
@@ -0,0 +1,9 @@
+using Sysytem;
+
+namespace HelloWorld{
+    public class Program_1_4{
+        public static void Main(string[] args){
+            Console.WrteLne("Hello World!");
+        }
+    }
+}

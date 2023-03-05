cbcf19b0386c71c53bd680911bb551b34e11a993
Karina Zubko <karinazubko2017@gmail.com>
2023-03-05 19:29:29 +0100

    Class example

---
+++ Program_3_3.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace classExample{
+    public class Program3_3{
+        public static void Main(string[] args){
+            Console.WriteLine(Counter.TotalCount);
+        }
+
+        public class Counter{
+            private int _count;
+            private static int _totalCount;
+
+            public int GetNextValue(){
+                _count +=1;
+                _totalCount +=1;
+                return _count;
+            }
+
+            public static int TotalCount => _totalCount;
+        }
+    }
+}

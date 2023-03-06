f505e935096f36c8288b11513bd6e5b8effbe735
Karina Zubko <karinazubko2017@gmail.com>
2023-03-06 20:19:58 +0100

    static constructor

---
+++ Program_3_31.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Data;
+
+class Program_3_31{
+    static void Main(string[] args){
+        //
+    }
+
+    public class Bar{
+        private static DateTime _firstUser;
+        static Bar(){
+            Console.WriteLine("Bar`s static constructor");
+            _firstUser = DateTime.Now;
+
+        }
+    }
+}
+

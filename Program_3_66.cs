be20742d1f5eaf4a5ceacc2567c1cc921f0d402b
Karina Zubko <karinazubko2017@gmail.com>
2023-03-07 14:02:54 +0100

    Properties

---
+++ Program_3_66.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Properties.Simple{
+    public class Calculated {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Magnitude {
+            get{
+                return Math.Sqrt(X * X + Y * Y);
+            }
+        }
+    }
+}
